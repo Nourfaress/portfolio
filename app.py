30d60c5bfd8a7fdd7a8eea1cea4a81b0518cecc4
Nour Fares <98664707+Nourfaress@users.noreply.github.com>
2022-09-03 22:24:31 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,  render_template
+
+
+app = Flask(__name__)
+
+
+# Index
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+# Hireme
+@app.route('/hireme')
+def about():
+    return render_template('hireme.html')
+
+# porjects
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+
+if __name__ == '__main__':
+    app.secret_key='secret1234'
+    app.run(app.run(host='0.0.0.0', debug=True, port=5000))
